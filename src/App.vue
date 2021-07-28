923710096bf3d56f7dbec9dc6385d0b17396a365
Haziq Hapiz <zyqq.dev@gmail.com>
2021-07-28 22:24:08 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- center a div -->
+  <!-- style a div to add margin top 50px -->
+  <Profile msg="Haziq Hapiz"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Profile from './components/Profile..vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Profile
   }
 }
 </script>
 
 <style>
+body {
+  background-image: url(./assets/img/bg.jpg);
+  background-size: cover;
+  /* background-repeat: inherit; */
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Profile..vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="max-w-2xl px-6 text-center mx-auto" id="profile">
+      <h1>TEXT</h1>
+        <h1>
+          Hi, I'm 
+            <span class="bg-indigo-600 text-white rounded px-1">TEXT</span>
+          <strong>
+            <slot name="title">{{ msg }}</slot>
+          </strong>,
+          <br> a Full Stack Developer.
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+* {
+  text-align: center;
+  color: white;
+  font-size: 1.5rem;
+}
+#profile {
+  position: relative;
+  width: 622px;
+  height: 645px;
+
+  background: linear-gradient(137.08deg, #5B22FF 0%, rgba(91, 34, 255, 0) 100%);
+  box-shadow: 0px 0px 25px 3px rgba(0, 71, 255, 0.85);
+  border-radius: 50px;
+}
+</style>
