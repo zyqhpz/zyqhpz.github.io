6bfd1c03f523eabcd2164786073854eff921dbbb
Haziq Hapiz <zyqq.dev@gmail.com>
2021-09-03 22:33:43 +0800

    add yolo img

--- src/components/CardTail.vue
+++ src/components/CardTail.vue
@@ -50,25 +50,24 @@
                      <span class="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#photography</span>
                      <span class="inline-block bg-orange-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#travel</span>
                      <span class="inline-block bg-purple-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#winter</span>
+                     <span class="inline-block bg-blue-600 rounded-md px-3 py-1 text-sm font-bold text-white mr-2 mb-2">CSS3</span>
+
+                     <c-badge c-badge ml="2" font-size="1em" variant="solid" bg="#800080">PHP</c-badge>
                   </div>
                </div>
             </div>
             <div>
                <div class="max-w-xs overflow-hidden bg-white rounded-2xl shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-                  <img class="w-full" src="https://picsum.photos/600/300/?image=13" alt="Sunset in the mountains">
+                  <img class="w-full" src="../assets/img/cards/yolo.png" alt="Sunset in the mountains">
                   <div class="px-6 py-4">
-                     <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                     <div class="font-bold text-xl mb-2">Car Counter</div>
                      <p class="text-gray-700 text-base">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                      </p>
                   </div>
                   <div class="px-6 pt-4 pb-2">
-                     <span class="inline-block bg-blue-600 rounded-md px-3 py-1 text-sm font-bold text-white mr-2 mb-2">CSS3</span>
-                     <span class="inline-block bg-orange-600 rounded-full px-3 py-1 text-sm font-bold text-white mr-2 mb-2">#travel</span>
-                     <span class="inline-block bg-purple-600 rounded-full px-3 py-1 text-sm font-bold text-white mr-2 mb-2">#winter</span>
                      <span class="inline-block rounded-full px-3 py-1 text-sm font-bold text-white mr-2 mb-2 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">Python</span>
                      <span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-bold text-white mr-2 mb-2">Computer Vision</span>
-                     <c-badge c-badge ml="2" font-size="1em" variant="solid" bg="#800080">PHP</c-badge>
 
                   </div>
                </div>
