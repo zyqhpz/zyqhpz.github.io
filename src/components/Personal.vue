0982bca9f9f2fcc937f5f8b11fe6c4ec25747dff
Haziq Hapiz <zyqq.dev@gmail.com>
2023-07-10 23:40:09 +0800

    refactor the work experience in json data

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -4,15 +4,15 @@
             <div class="p-10 md:p-12 text-gray-700 text-md">I am <b>adaptive, independent, and a fast learner</b>. I am capable of learning new tech stacks and applying them to develop a project <b>within two weeks</b>. I am passionate about web development, specifically <b>back-end development</b>. I love to code and am an enthusiast for learning new things.</div>
         </div>
         <div id="box" class="mx-auto mt-12 max-w-xs md:max-w-md h-auto px-4 py-8 sm:p-8 bg-gray-200 text-gray-700">
-            <div class="text-md mb-4">Favourite Tech Stack:</div>
+            <div class="text-md mb-4">Preferred Tech Stack:</div>
             <div class="text-sm">
-                <b>Back-End:</b> PHP (Laravel)
+                <b>Back-End:</b> PHP (Laravel), Go
             </div>
             <div class="text-sm">
-                <b>Front-End:</b> VueJS
+                <b>Front-End:</b> Vue, React
             </div>
             <div class="text-sm">
-                <b>Styling:</b> Tailwind
+                <b>Styling:</b> Tailwind, Bootstrap
             </div>
             <div class="text-sm">
                 <b>Server Management Tools:</b> AWS, RunCloud
--- src/components/WorkingExperience.vue
+++ src/components/WorkingExperience.vue
@@ -2,35 +2,19 @@
     <div class="items-center justify-center mt-16 max-w-2xl mx-auto" id="aboutBox">
         <div id="working">
             <span class="text-2xl font-semibold text-gray-200">Working Experience</span>
-            <div class="bg-gray-200 rounded-lg p-4 mt-8">
+            <div v-for="item in work" :key="item.id" class="bg-gray-200 rounded-lg p-4 mt-8">
                 <div class="my-2">
                     <div class="text-gray-700">
                     <div class="font-medium">Company:</div>
-                    <div class="text-sm">Datascience Sdn Bhd</div>
+                    <div class="text-sm">{{ item.company }}</div>
                     </div>
                     <div class="mt-2 text-gray-700">
                     <div class="font-medium">Position:</div>
-                    <div class="text-sm">Internship</div>
+                    <div class="text-sm">{{ item.position }}</div>
                     </div>
                     <div class="mt-2 text-gray-700">
                     <div class="font-medium">Duration:</div>
-                    <div class="text-sm">August 2022 - October 2022</div>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-gray-200 rounded-lg p-4 mt-8">
-                <div class="my-2">
-                    <div class="text-gray-700">
-                    <div class="font-medium">Company:</div>
-                    <div class="text-sm">Lean Giap Group Of Companies</div>
-                    </div>
-                    <div class="mt-2 text-gray-700">
-                    <div class="font-medium">Position:</div>
-                    <div class="text-sm">Freelance (PHP Programmer)</div>
-                    </div>
-                    <div class="mt-2 text-gray-700">
-                    <div class="font-medium">Duration:</div>
-                    <div class="text-sm">December 2021 - February 2022</div>
+                    <div class="text-sm">{{ item.duration }}</div>
                     </div>
                 </div>
             </div>
@@ -41,7 +25,14 @@
 </style>
 <script>
 
+import data from '@/assets/work.json'
+
 export default {
     name: 'WorkingExperience',
+    data() {
+        return {
+            work: data
+        }
+    },
 }
 </script>
