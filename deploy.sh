2489be71e1a1262826fdabef8860296c08c2fb2a
Haziq Hapiz <zyqq.dev@gmail.com>
2024-03-13 23:28:45 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,21 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# Abort script on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 
-git config --local user.name "Haziq Hapiz"
-git config --local user.email "zyqq.dev@gmail.com"
+git config user.name "Haziq Hapiz"
+git config user.email "zyqq.dev@gmail.com"
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:zyqhpz/zyqhpz.github.io.git master:gh-pages
-# git push -f git@github.com:zyqhpz/zyqhpz.github.io.git master:deployment
+git remote add origin https://github.com/zyqhpz/zyqhpz.github.io.git
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f origin master:gh-pages
 
-cd -
+cd ..
