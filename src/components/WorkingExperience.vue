3231e9fbe870198d2b008158ab9bae50f7d861bf
Haziq Hapiz <zyqq.dev@gmail.com>
2024-05-05 21:15:38 +0800

    add latest job

--- src/components/WorkingExperience.vue
+++ src/components/WorkingExperience.vue
@@ -5,16 +5,17 @@
             <div v-for="item in work" :key="item.id" class="bg-gray-200 rounded-lg p-4 mt-8">
                 <div class="my-2">
                     <div class="text-gray-700">
-                    <div class="font-medium">Company:</div>
-                    <div class="text-sm">{{ item.company }}</div>
+                        <div class="font-medium">Company:</div>
+                        <a v-if="item.link" class="text-sm no-underline text-gray-700 hover:text-blue-500 hover:underline hover:font-medium" :href="item.link" target="_blank">{{ item.company }}</a>
+                        <a v-else class="text-sm no-underline text-gray-700">{{ item.company }}</a>
                     </div>
                     <div class="mt-2 text-gray-700">
-                    <div class="font-medium">Position:</div>
-                    <div class="text-sm">{{ item.position }}</div>
+                        <div class="font-medium">Position:</div>
+                        <div class="text-sm">{{ item.position }}</div>
                     </div>
                     <div class="mt-2 text-gray-700">
-                    <div class="font-medium">Duration:</div>
-                    <div class="text-sm">{{ item.duration }}</div>
+                        <div class="font-medium">Duration:</div>
+                        <div class="text-sm">{{ item.duration }}</div>
                     </div>
                 </div>
             </div>
