7b3c62b00257e068350badaef7507a0a63beb5b3
Haziq Hapiz <zyqq.dev@gmail.com>
2024-07-07 13:18:56 +0800

    fix: extra space in hero intro

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="flex flex-col h-full text-center text-gray-700 dark:text-gray-200 m-8">
-        <div class="m-auto">
-            <h2 class="text-xl mb-2 md:text-2xl lg:text-3xl font-semibold underline">
-                <span class="font-normal dark:text-gray-300">Hi, I'm </span>Haziq Hapiz
-            </h2>
-            <p class="text-md lg:text-lg font-normal">
-                <span class="inline-block">An aspiring Software Engineer from Malaysia&nbsp;
-                    <IconMalaysia class="inline-block" />&nbsp;with a passion to make meaningful impacts!
-                </span>
-            </p>
-            <Connect />
-            <TechStack />
-        </div>
+  <div
+    class="flex flex-col h-full text-center text-gray-700 dark:text-gray-200 m-8"
+  >
+    <div class="m-auto">
+      <h2 class="text-xl mb-2 md:text-2xl lg:text-3xl font-semibold underline">
+        <span class="font-normal dark:text-gray-300">Hi, I'm </span>Haziq Hapiz
+      </h2>
+      <p class="text-md lg:text-lg font-normal">
+        <span class="inline-block"
+          >An aspiring Software Engineer from Malaysia&nbsp;<IconMalaysia
+            class="inline-block"
+          />&nbsp;with a passion to make meaningful impacts!
+        </span>
+      </p>
+      <Connect />
+      <TechStack />
     </div>
+  </div>
 </template>
